5c7db47aebed00effb9b97f0529f9808362d2b93
Joan Currie <jecurrie8@gmail.com>
2020-03-22 15:57:16 -0400

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-=begin
+#=begin
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -12,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
-=end
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -24,4 +24,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
   end
+  
 end
