5e56c4c05e0267e89edfd07aa6aeb98062c60a99
Joan Currie <jecurrie8@gmail.com>
2020-03-23 04:28:14 -0400

    Finish user edit, update, index, and destroy actions

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-#=begin
+=begin
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -25,5 +25,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert is_logged_in?
   end
-  
+=end  
 end
