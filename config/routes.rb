24b70223dcb52edbaf12e0121154c82f3de1ded5
Joan Currie <jecurrie8@gmail.com>
2020-03-25 22:37:29 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root 'application#hello'  #  root   'static_pages#home'
-=begin
+  #root 'application#hello'  #  
+  root   'static_pages#home'
+  #get  'static_pages/home'
+
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
+=begin  
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
