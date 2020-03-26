11520dc72fa9555fbe0b83698c3d417afefed515
Joan Currie <jecurrie8@gmail.com>
2020-03-25 22:32:16 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root   'static_pages#home'
+  root 'application#hello'  #  root   'static_pages#home'
 =begin
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
