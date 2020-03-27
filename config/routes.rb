5c729273863cd17abbea0f663f9e4236108f34d9
Joan Currie <jecurrie8@gmail.com>
2020-03-26 23:50:47 -0400

    Ready to deploy v1.0

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
 
-  get    '/signup',  to: 'users#new'  #Listing 5.43: A route for the signup page.
+  ## ##get    '/signup',  to: 'users#new'  #Listing 5.43: A route for the signup page.
 =begin 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
