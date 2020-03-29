d4aec095275509e1b59a65d2e69dc5a9e946739a
Joan Currie <jecurrie8@gmail.com>
2020-03-29 01:24:14 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  root 'static_pages#home' 
-  get  'static_pages/home'  #Listing 3.8: The routes for the home and help actions in the Static Pages controller. config/routes.rb
-  get  'static_pages/help'
-
-  get  'static_pages/about' #Listing 3.20: Adding the about route. red  config/routes.rb
-  get  'static_pages/contact' #Listing 5.23: Adding a route for the Contact page. 
-
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+end
 #  root 'application#hello'  # Listing 3.6: Setting the root route. 
 =begin   
   root   'static_pages#home'
@@ -25,4 +24,4 @@ Rails.application.routes.draw do
 ##  resources :password_resets,     only: [:new, :create, :edit, :update]
 #  resources :microposts,          only: [:create, :destroy]
 =end
-end
+
