164775e2b22f00c4e93c9913e3007544d5ed086f
Joan Currie <jecurrie8@gmail.com>
2020-03-30 14:13:31 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,9 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users  
 end
 #  root 'application#hello'  # Listing 3.6: Setting the root route. 
-=begin   
-  root   'static_pages#home'
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-
-  get    '/signup',  to: 'users#new'  #Listing 5.43: A route for the signup page.
-  resources :users
-=end
 #  get    '/login',   to: 'sessions#new'
 #  post   '/login',   to: 'sessions#create'
 =begin
