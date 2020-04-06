c2eed5bcc66b5994081ac6ea900cc49c7b3e567b
Joan Currie <jecurrie8@gmail.com>
2020-04-05 23:45:13 -0400

    Ready to deploy v1.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,7 @@ User.create!(name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
-  admin:     true,
-  activated: true,
-  activated_at: Time.zone.now)
+  admin: true)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -13,9 +11,7 @@ name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
 User.create!(name:  name,
-   email: email,
-   password:              password,
-   password_confirmation: password,
-   activated: true,
-   activated_at: Time.zone.now)
+    email: email,
+    password:              password,
+    password_confirmation: password)
 end
