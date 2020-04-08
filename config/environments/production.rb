3325d2b162527982e9414dd2c933efd9944d2ea2
Joan Currie <jecurrie8@gmail.com>
2020-04-08 00:08:50 -0400

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on Amazon AWS.
-  ## ##config.active_storage.service = :amazon
+  config.active_storage.service = :amazon
   host = 'localhost:3000'                     # Local server
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
