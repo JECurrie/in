1fdf1b7cb8f91086e5a38f1f2db14bfa7e324241
Joan Currie <jecurrie8@gmail.com>
2020-04-07 23:10:54 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
@@ -59,7 +63,22 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
   end
-  
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
     # Converts email to all lower-case.
