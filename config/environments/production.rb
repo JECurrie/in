1687b64ec832862210a4ea4e10b86efec555cfdf
Joan Currie <jecurrie8@gmail.com>
2020-04-10 19:47:22 -0400

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sleepy-badlands-85818.herokuapp.com'
+  host = 'https://fast-taiga-22147.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
